888975e05a4ffda4e3b0e6b079793b6b0e9282e6
Renato Moreno <renats.mc@gmail.com>
2021-12-26 19:46:10 -0300

    ativa produtos na página de orçamento

--- js/script.js
+++ js/script.js
@@ -12,4 +12,14 @@ function ativarLink(link) {
 
 links.forEach(ativarLink);
 
-//faz com que ao clicar em comprar alguma bicicleta ou seguro, já venha selecionado o mesmo na página de orçamento
+// faz com que ao clicar em comprar alguma bicicleta ou seguro, já venha selecionado o mesmo na página de orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
