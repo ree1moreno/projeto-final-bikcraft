7291323544f175ad4666427cda3a389e224a3274
Renato Moreno <renats.mc@gmail.com>
2021-12-26 20:42:25 -0300

    galeria de bicicletas

--- js/script.js
+++ js/script.js
@@ -42,3 +42,21 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// galeria de bicicletas
+const galeria = document.querySelectorAll(".bicicleta-img img");
+const galeriaContainer = document.querySelector(".bicicleta-img");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = window.matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(imagem) {
+  imagem.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
